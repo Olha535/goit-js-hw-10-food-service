df9febae870f31da876d1ae2ad8ff61ba74405dc
Olha Tarasova <tarasovao0505@gmail.com>
2021-07-22 17:45:59 +0300

    Переход по темам

--- src/js/light-theme-box.js
+++ src/js/light-theme-box.js
@@ -5,24 +5,27 @@ const Theme = {
 
 //document.body.classList.add(Theme.LIGHT);
 
-const value = localStorage.getItem('theme');
-if (value === 'dark-theme') {
-  themeSwitchToggleEl.checked = true;
-  body.classList.add('dark-theme');
-}
+const body = document.querySelector('body');
+body.classList.add(Theme.LIGHT);
 
 const themeSwitchToggleEl = document.querySelector('.theme-switch__toggle');
 themeSwitchToggleEl.addEventListener('change', onThemeSwitch);
 
 function onThemeSwitch(evt) {
   if (evt.target.checked) {
-    localStorage.setItem('theme', Theme.Dark);
+    localStorage.setItem('theme', Theme.DARK);
     //localStorage.removeItem('theme');
+    document.body.classList.toggle(Theme.DARK);
   } else {
     localStorage.setItem('theme', Theme.LIGHT);
+    document.body.classList.toggle(Theme.LIGHT);
   }
-  document.body.classList.toggle(Theme.LIGHT);
-  document.body.classList.toggle(Theme.Dark);
+}
+
+const value = localStorage.getItem('theme');
+if (value === 'dark-theme') {
+  themeSwitchToggleEl.checked = true;
+  body.classList.add('dark-theme');
 }
 
 //function onDarkTheme() {
